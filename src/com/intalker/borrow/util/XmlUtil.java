294b55b10abfad01002ff91ba58bc23c1d076661
tomdong <jiang2913@hotmail.com>
2013-01-07 15:13:56 +0800

    Support save/load for more info.

--- src/com/intalker/borrow/util/XmlUtil.java
+++ src/com/intalker/borrow/util/XmlUtil.java
@@ -23,6 +23,11 @@ public class XmlUtil {
 	private static final String BooksNodeName = "books";
 	private static final String BookNodeName = "book";
 	private static final String ISBNAttributeName = "isbn";
+	private static final String BookNameNodeName = "name";
+	private static final String AuthorNodeName = "author";
+	private static final String PublisherNodeName = "publisher";
+	private static final String PageCountNodeName = "pagecount";
+	private static final String DescriptionNodeName = "description";
 //	public static void save(ArrayList<BookInfo> bookInfoList, OutputStream out)
 //			throws Exception {
 //		XmlSerializer serializer = Xml.newSerializer();
@@ -56,15 +61,76 @@ public class XmlUtil {
 			Document doc = builder.parse(cachedXmlFilePath);
 			Element rootElement = doc.getDocumentElement();
 			NodeList items = rootElement.getElementsByTagName(BookNodeName);
-			for (int i = 0; i < items.getLength(); i++) {
+			int nodeCount = items.getLength();
+			for (int i = 0; i < nodeCount; ++i) {
 				Node item = items.item(i);
+				if(null == item)
+				{
+					continue;
+				}
 				NamedNodeMap attributes = item.getAttributes();
 				// NodeList properties = item.getChildNodes();
 
 				Node isbnNode = attributes.getNamedItem(ISBNAttributeName);
 				String isbn = isbnNode.getNodeValue();
-
+				
 				BookInfo book = new BookInfo(isbn);
+
+				NodeList subNodes = item.getChildNodes();
+				int subNodeCount = subNodes.getLength();
+				for(int j = 0; j < subNodeCount; ++j)
+				{
+					Node subNode = subNodes.item(j);
+					if(null == subNode)
+					{
+						continue;
+					}
+					
+					String nodeTagName = subNode.getNodeName();
+					if (nodeTagName.compareTo(BookNameNodeName) == 0) {
+						String bookName = subNode.getTextContent();
+						book.setName(bookName);
+					}
+					
+					String authorTagName = subNode.getNodeName();
+					if (authorTagName.compareTo(AuthorNodeName) == 0) {
+						String author = subNode.getTextContent();
+						book.setAuthor(author);
+					}
+					
+					String publisherTagName = subNode.getNodeName();
+					if (publisherTagName.compareTo(PublisherNodeName) == 0) {
+						String publisher = subNode.getTextContent();
+						book.setPublisher(publisher);
+					}
+					
+					String pageCountTagName = subNode.getNodeName();
+					if (pageCountTagName.compareTo(PageCountNodeName) == 0) {
+						String pageCount = subNode.getTextContent();
+						book.setPageCount(pageCount);
+					}
+					
+					String descriptionTagName = subNode.getNodeName();
+					if (descriptionTagName.compareTo(DescriptionNodeName) == 0) {
+						String description = subNode.getTextContent();
+						book.setDescription(description);
+					}
+				}
+//				Element authorElement = doc.createElement(AuthorNodeName);
+//				nameElement.setTextContent(book.getAuthor());
+//				bookElement.appendChild(authorElement);
+//				
+//				Element publisherElement = doc.createElement(PublisherNodeName);
+//				nameElement.setTextContent(book.getPublisher());
+//				bookElement.appendChild(publisherElement);
+//				
+//				Element pageCountElement = doc.createElement(PageCountNodeName);
+//				nameElement.setTextContent(book.getPageCount());
+//				bookElement.appendChild(pageCountElement);
+//				
+//				Element descriptionElement = doc.createElement(DescriptionNodeName);
+//				nameElement.setTextContent(book.getDescription());
+//				bookElement.appendChild(descriptionElement);
 
 				books.add(book);
 			}
@@ -83,6 +149,32 @@ public class XmlUtil {
 		for (BookInfo book : books) {
 			Element bookElement = doc.createElement(BookNodeName);
 			bookElement.setAttribute(ISBNAttributeName, book.getISBN());
+			
+//			private static final String BookNameNodeName = "name";
+//			private static final String AuthorNodeName = "author";
+//			private static final String PublisherNodeName = "publisher";
+//			private static final String PageCountNodeName = "pagecount";
+//			private static final String DescriptionNodeName = "description";
+			
+			Element nameElement = doc.createElement(BookNameNodeName);
+			nameElement.setTextContent(book.getName());
+			bookElement.appendChild(nameElement);
+			
+			Element authorElement = doc.createElement(AuthorNodeName);
+			authorElement.setTextContent(book.getAuthor());
+			bookElement.appendChild(authorElement);
+			
+			Element publisherElement = doc.createElement(PublisherNodeName);
+			publisherElement.setTextContent(book.getPublisher());
+			bookElement.appendChild(publisherElement);
+			
+			Element pageCountElement = doc.createElement(PageCountNodeName);
+			pageCountElement.setTextContent(book.getPageCount());
+			bookElement.appendChild(pageCountElement);
+			
+			Element descriptionElement = doc.createElement(DescriptionNodeName);
+			descriptionElement.setTextContent(book.getDescription());
+			bookElement.appendChild(descriptionElement);
 			
 //			Element nameElement = doc.createElement("name");
 //			nameElement.setTextContent(book.getName());
