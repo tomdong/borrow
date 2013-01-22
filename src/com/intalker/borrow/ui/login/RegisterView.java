552e3fe666d5f98642460be33b94e0b29b550cb6
ryan shao <ryan.shao@autodesk.com>
2013-01-22 13:18:43 +0800

    let register work

--- src/com/intalker/borrow/ui/login/RegisterView.java
+++ src/com/intalker/borrow/ui/login/RegisterView.java
@@ -1,24 +1,39 @@
 package com.intalker.borrow.ui.login;
 
 import com.intalker.borrow.R;
+import com.intalker.borrow.cloud.CloudAPI;
+import com.intalker.borrow.cloud.CloudAPIAsyncTask.ICloudAPITaskListener;
+import com.intalker.borrow.data.UserInfo;
 import com.intalker.borrow.util.DensityAdaptor;
 import com.intalker.borrow.util.LayoutUtil;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.text.Editable;
 import android.text.InputType;
+import android.text.TextWatcher;
 import android.view.Gravity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 import android.widget.ImageView.ScaleType;
 import android.widget.RelativeLayout.LayoutParams;
 import android.widget.TextView;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.InputMethodManager;
 
-public class RegisterView extends LinearLayout{
+public class RegisterView extends LinearLayout implements OnClickListener{
+	
+	public interface OnRegisterListener
+	{
+		public abstract void onSuccess();
+		public abstract void onBack();
+	}
 //	private final static int LEFTMARGIN = 20;
 //	private final static int TOPMARGIN = 20;
 //	private final static int BETWEENMARGIN = 5;
@@ -27,6 +42,8 @@ public class RegisterView extends LinearLayout{
 	private TextView mTitleBack = null;
 	private TextView mNameLabel = null;
 	private EditText mNameInput = null;
+	private TextView mNickNameLabel = null;
+	private EditText mNickNameInput = null;
 	private TextView mPasswordLabel = null;
 	private EditText mPasswordInput = null;
 	private TextView mPasswordConfirmLabel = null;
@@ -35,12 +52,22 @@ public class RegisterView extends LinearLayout{
 	private CheckBox mAgreementCheck = null;
 	
 	private Button 	 mRegisterButton = null;
+	
+	private String mName = "";
+	private String mPassword = "";
+	private String mConfirmPassword = "";
+	
+	private OnRegisterListener mRegListener = null;
 	public RegisterView(Context context) {
 		super(context);
 		
 		init(context);
 	}
 	
+	public void setRegisterListener(OnRegisterListener l)
+	{
+		mRegListener = l;
+	}
 	
 	private void init(Context c)
 	{
@@ -69,13 +96,22 @@ public class RegisterView extends LinearLayout{
 		mTitleBack.setText(R.string.reg_back);
 		mTitleBack.setTextSize(12);
 		mTitleBack.setTextColor(Color.WHITE);
-		mTitleBack.setLayoutParams(rpwwl);		
+		mTitleBack.setLayoutParams(rpwwl);	
+		mTitleBack.setOnClickListener(new OnClickListener()
+		{
+			@Override
+			public void onClick(View v) {
+				hide_keyboard();
+				if(mRegListener!=null)
+					mRegListener.onBack();
+			}
+		});
 		titleLine.addView(mTitleBack);	
 		mTitleLabel = new TextView(c);
 		mTitleLabel.setText(R.string.reg_title);
 		mTitleLabel.setTextSize(12);
 		mTitleLabel.setTextColor(Color.WHITE);	
-		mTitleLabel.setLayoutParams(rpwwc);		
+		mTitleLabel.setLayoutParams(rpwwc);	
 		titleLine.addView(mTitleLabel);
 		
 		mNameLabel = new TextView(c);
@@ -85,7 +121,46 @@ public class RegisterView extends LinearLayout{
 		mNameInput = new EditText(c);
 		mNameInput.setLayoutParams(lpfw);
 		mNameInput.setInputType(InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
+		mNameInput.setHint(R.string.reg_name_hint);
+		mNameInput.addTextChangedListener(new TextWatcher()
+		{
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count,
+					int after) {				
+			}
+
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before,
+					int count) {				
+			}
+
+			@Override
+			public void afterTextChanged(Editable s) {
+				String inputname = mNameInput.getText().toString();
+				if(!isValidName(inputname))
+				{
+					mNameInput.setTextColor(Color.RED);
+					mRegisterButton.setEnabled(false);
+				}	
+				else
+				{
+					mNameInput.setTextColor(Color.BLACK);
+					mName = inputname;
+					tryEnableRegButton();
+				}
+			}
+		});
 		this.addView(mNameInput);
+		
+		mNickNameLabel = new TextView(c);
+		mNickNameLabel.setText(R.string.reg_nickname); 
+		mNickNameLabel.setLayoutParams(lpww);
+		this.addView(mNickNameLabel);
+		mNickNameInput = new EditText(c);
+		mNickNameInput.setLayoutParams(lpfw);
+		mNickNameInput.setHint(R.string.reg_nickname_hint);
+		
+		this.addView(mNickNameInput);
 		
 		mPasswordLabel = new TextView(c);
 		mPasswordLabel.setText(R.string.reg_passowrd); 
@@ -95,6 +170,35 @@ public class RegisterView extends LinearLayout{
 		mPasswordInput.setLayoutParams(lpfw);
 		mPasswordInput.setInputType(InputType.TYPE_CLASS_TEXT
 				| InputType.TYPE_TEXT_VARIATION_PASSWORD);
+		mPasswordInput.setHint(R.string.reg_password_hint);
+		mPasswordInput.addTextChangedListener(new TextWatcher()
+		{
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count,
+					int after) {				
+			}
+
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before,
+					int count) {				
+			}
+
+			@Override
+			public void afterTextChanged(Editable s) {
+				String inputpass = mPasswordInput.getText().toString();
+				if(!isValidPassword(inputpass))
+				{
+					mPasswordInput.setTextColor(Color.RED);
+					mRegisterButton.setEnabled(false);
+				}	
+				else
+				{
+					mPasswordInput.setTextColor(Color.BLACK);
+					mPassword = inputpass;
+					tryEnableRegButton();
+				}
+			}
+		});
 		this.addView(mPasswordInput);
 		
 		mPasswordConfirmLabel = new TextView(c);
@@ -105,11 +209,40 @@ public class RegisterView extends LinearLayout{
 		mPasswordConfirmInput.setLayoutParams(lpfw);
 		mPasswordConfirmInput.setInputType(InputType.TYPE_CLASS_TEXT
 				| InputType.TYPE_TEXT_VARIATION_PASSWORD);
+		mPasswordConfirmInput.addTextChangedListener(new TextWatcher()
+		{
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count,
+					int after) {				
+			}
+
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before,
+					int count) {				
+			}
+
+			@Override
+			public void afterTextChanged(Editable s) {
+				String input = mPasswordConfirmInput.getText().toString();
+				if(!isValidPassword(input))
+				{
+					mPasswordConfirmInput.setTextColor(Color.RED);
+					mRegisterButton.setEnabled(false);
+				}	
+				else
+				{
+					mPasswordConfirmInput.setTextColor(Color.BLACK);
+					mConfirmPassword = input;
+					tryEnableRegButton();
+				}
+			}
+		});
 		this.addView(mPasswordConfirmInput);
 		
 		mAgreementCheck = new CheckBox(c);
 		mAgreementCheck.setLayoutParams(lpww);
 		mAgreementCheck.setText(R.string.reg_agreement);
+		mAgreementCheck.setChecked(true);
 		this.addView(mAgreementCheck);
 		
 		ImageView sep = new ImageView(this.getContext());
@@ -121,6 +254,68 @@ public class RegisterView extends LinearLayout{
 		mRegisterButton = new Button(c);
 		mRegisterButton.setText(R.string.reg_commit); 
 		mRegisterButton.setLayoutParams(lpfw);
+		mRegisterButton.setOnClickListener(this);
+		mRegisterButton.setEnabled(false);
 		this.addView(mRegisterButton);
+	}
+	
+	private boolean isValidName(String s)
+	{
+		if(s.contains(" ") ||!s.contains("@"))
+			return false;
+		
+		return true;
+	}
+	
+	private boolean isValidPassword(String s)
+	{
+		if(s.contains(" "))
+			return false;
+		if(s.length()<5 || s.length()>20)
+			return false;
+		
+		return true;
+	}
+	private void tryEnableRegButton()
+	{
+		if(mPassword.contentEquals(mConfirmPassword) && this.mAgreementCheck.isChecked())
+			mRegisterButton.setEnabled(true);
+	}
+
+
+	@Override
+	public void onClick(View v) {
+		 CloudAPI.signUp(v.getContext(),
+		 mName,
+		 mPassword,
+		 mNickNameInput.getText().toString(),
+		 new ICloudAPITaskListener(){
+		
+			 @Override
+			 public void onFinish(int returnCode) {
+				 switch (returnCode) {
+					case CloudAPI.Return_OK:
+						hide_keyboard();
+						if(mRegListener!=null)
+							mRegListener.onSuccess();
+						break;
+					case CloudAPI.Return_UserNameOccupied:
+						Toast.makeText(mNameInput.getContext(), "User name occupied.", Toast.LENGTH_SHORT)
+								.show();
+						break;
+					case CloudAPI.Return_NetworkError:
+						Toast.makeText(mNameInput.getContext(), "Network error.", Toast.LENGTH_SHORT).show();
+						break;
+					default:
+						Toast.makeText(mNameInput.getContext(), "Unknown error.", Toast.LENGTH_SHORT).show();
+						break;
+				 }	
+			 }		
+		 });
+	}
+
+	private void hide_keyboard() {
+		InputMethodManager imManager = (InputMethodManager)this.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+		imManager.hideSoftInputFromWindow(this.getApplicationWindowToken(), 0, null);
 	}
 }
