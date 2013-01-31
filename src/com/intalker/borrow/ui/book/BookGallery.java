tomdong/borrow

commit 2772a506fe764ea59e9f91e817f3287f7d9955af

Fix bug #58.

src/com/intalker/borrow/ui/book/BookGallery.java
@@ -12,11 +12,13 @@ import com.intalker.borrow.data.UserInfo;
 import com.intalker.borrow.isbn.ISBNResolver;
 import com.intalker.borrow.ui.control.HaloButton;
 import com.intalker.borrow.util.DensityAdaptor;
+import com.intalker.borrow.util.DeviceUtil;
 import com.intalker.borrow.util.LayoutUtil;
 import com.intalker.borrow.util.ScanUtil;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -215,6 +217,18 @@ public class BookGallery extends RelativeLayout {
 		shelfViewLP.topMargin = LayoutUtil.getGalleryTopPanelHeight();
 		shelfViewLP.bottomMargin = LayoutUtil.getGalleryBottomPanelHeight();
 		this.addView(mShelfView, shelfViewLP);
+		
+		if(DeviceUtil.isFroyo())
+		{
+			mShelfView.setOnTouchListener(new OnTouchListener(){
+
+				@Override
+				public boolean onTouch(View v, MotionEvent event) {
+					return true;
+				}
+				
+			});
+		}
 	}
 	
 	public void clearBooks()

src/com/intalker/borrow/ui/book/BookShelfView.java
@@ -3,7 +3,7 @@ package com.intalker.borrow.ui.book;
 import java.util.ArrayList;
 import com.intalker.borrow.R;
 import com.intalker.borrow.data.BookInfo;
-import com.intalker.borrow.ui.control.DragRefreshScroll;
+//import com.intalker.borrow.ui.control.DragRefreshScroll;
 import com.intalker.borrow.util.LayoutUtil;
 
 import android.content.Context;

commit 35c32c014a8b9829b4028333152421c99c445fdf

Fix a regression caused by 2772a506fe764ea59e9f91e817f3287f7d9955af.

src/com/intalker/borrow/ui/book/BookGallery.java
@@ -220,7 +220,7 @@ public class BookGallery extends RelativeLayout {
 		
 		if(DeviceUtil.isFroyo())
 		{
-			mShelfView.setOnTouchListener(new OnTouchListener(){
+			this.setOnTouchListener(new OnTouchListener(){
 
 				@Override
 				public boolean onTouch(View v, MotionEvent event) {