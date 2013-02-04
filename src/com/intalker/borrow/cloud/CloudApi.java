cbccbb1c20f2bc756a811de0dd999119b6e6a727
tomdong <jiang2913@hotmail.com>
2013-02-04 09:24:50 +0800

    Fix defect #70.

--- src/com/intalker/borrow/cloud/CloudApi.java
+++ src/com/intalker/borrow/cloud/CloudApi.java
@@ -19,7 +19,7 @@ import com.intalker.borrow.util.DBUtil;
 import com.intalker.borrow.util.JSONUtil;
 
 public class CloudAPI {
-	public final static String API_BaseURL = "http://services.sketchbook.cn/test/openshelf/api?op=";
+	public final static String API_BaseURL = "http://services.sketchbook.cn/test/openshelf/api/index.php?op=";
 
 	// API operations
 	public final static String API_Login = "Login";
